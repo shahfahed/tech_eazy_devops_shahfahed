dfadaf6969dd35fd7c8f852057b38aa6879f35ac
Shah <shahfahed88@gmail.com>
2025-07-02 12:25:48 +0530

    updated tf with s3 blocks

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,14 +36,70 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "${var.name_tag}-ec2-logs"
+  # String interploation
+  tags = {
+    Name = var.name_tag+"-ec2-logs"
+    # Direct string interpolation from ver above 0.12
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = var.name_tag+"-ec2-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = {
+    tag-key = var.name_tag+"-ec2-role"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = var.name_tag+"-ec2-policy"
+  role = aws_iam_role.ec2_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.logs_bucket.bucket}/*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.name_tag}-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   user_data = templatefile("${path.module}/user_data.sh.tpl", {
-    repo_url = var.repo_url
+    repo_url    = var.repo_url
+    bucket_name = aws_se_bucket.logs_bucket.bucket
     })
 
   tags = {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "public_ip" {
     description = "EC2 public IP"
     value       = aws_instance.ec2.public_ip  
 }
+
+output "s3_bucket_name" {
+    description = "EC2 logs bucket"
+    value       = aws_s3_bucket.logs_bucket.bucket
+}
